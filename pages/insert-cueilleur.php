83632b713dacce9e559ac31952a323d7813cbf07
nekena123 <rakotomanjatonekena@gmail.com>
2024-02-12 13:39:33 +0200

    pages insert

--- pages/insert-cueilleur.php
+++ pages/insert-cueilleur.php
@@ -20,13 +20,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="../assets/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="../assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/style-insert.css" rel="stylesheet">
+
 </head>
 <body>
     <div class="container-fluid bg-white sticky-top" style="top: -150px;">
@@ -62,44 +64,48 @@
 
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeIn;">
         <div class="container text-center py-5">
-            <h1 class="display-2 text-dark mb-4  slideInDown">Parcelle</h1>
+            <h1 class="display-2 text-dark mb-4  slideInDown">Cueilleur</h1>
             <nav aria-label="breadcrumb animated slideInDown">
             </nav>
         </div>
     </div>
 
+    nom varchar(50),
+    idGenre int,
+    dateNaissance date,
+
     <div class="form-insert">
     <form action="#">
-            <label for="Nom">Numero parcelle</label>
-            <input type="number" id="numero" name="numero" value="" required>
+            <label for="Nom">Nom cueilleur</label>
+            <input type="text" id="nom" name="name" value="" required>
+            <br>
             
-            <label for="occupation">Surface</label>
-            <input type="text" id="occupation" name="occupation" value="" required>
+            <label for="occupation">Genre</label>
+            <input type="text" id="genre" name="genre" value="" required>
+            <br>
 
-            <label for="rendement">Variete</label>
-            <input type="text" id="variete" name="variete" value="" required>
+            <label for="rendement">Date de naissance</label>
+            <input type="text" id="dtn" name="date" value="" required>
+            <br>
 
             <button type="submit">Inserer</button>
     </form>
     </div>
 
-    numeroParcelle int unique,
-    surface decimal(11 , 2),
-    idVariete int,
-
     <table border='1'>
         <thead>
             <tr>
-                <th>Numero Parcelle</th>
-                <th>Surface</th>
-                <th>Variete</th>
+                <th>Nom variete</th>
+                <th>Occupation</th>
+                <th>Rendement</th>
+                <th>Acitons</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>Numero1</td>
-                <td>Surface1</td>
-                <td>Variete1</td>
+                <td>Nom1</td>
+                <td>Occupation1</td>
+                <td>Rendement1</td>
                 <td>
                     <a href="" class="glyphicon glyphicon-trash" id="icon"></a>
                     <a href="" class="glyphicon glyphicon-pencil" id="icon"></a>
