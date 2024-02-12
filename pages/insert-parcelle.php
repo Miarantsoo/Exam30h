582ef43611e8bdfdb5f316a7ba40c882e11b6334
nekena123 <rakotomanjatonekena@gmail.com>
2024-02-12 19:15:54 +0200

    final

--- pages/insert-parcelle.php
+++ pages/insert-parcelle.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-    <title>Tea House - Tea Shop Website Template</title>
+<title>Leaf</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
