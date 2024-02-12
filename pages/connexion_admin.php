4b4991dcaf0d7f6db0476cbe71c41a5c392fff19
nekena123 <rakotomanjatonekena@gmail.com>
2024-02-12 10:54:32 +0200

    page_connexion

---
+++ pages/connexion_admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login-container">
+        <h2>Connexion</h2>
+        <form action="#">
+            <label for="username">E-mail</label>
+            <input type="email" id="username" name="username" value="" required>
+            
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" value="" required>
+
+            <button type="submit">Se Connecter</button>
+
+            <p><a href="index.html">Se connecter en tant qu'utilisateur</a></p>
+
+        </form>
+    </div>
+</body>
+</html>
